59c6a9c1ef12e7c06d75901df551cb045894c992
Mateo <mortiz-2024204@kinal.edu.gt>
2026-04-08 10:04:26 -0600

    Feat: se agregaron mas funcionalidades del html y css

--- ProyectoTaller/ATC1-B2 (1)/EjercicioBim2/EjercicioBim2/src/main/java/com/mateoortiz/ejercicio/repository/ProductosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mateoortiz.ejercicio.repository;
-
-import com.mateoortiz.ejercicio.entity.Productos;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductosRepository extends JpaRepository<Productos, Integer> {
-}
--- ProyectoTaller/ATC1-B2 (1)/EjercicioBim2/EjercicioBim2/src/main/java/com/mateoortiz/ejercicio/repository/UsuariosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mateoortiz.ejercicio.repository;
-
-import com.mateoortiz.ejercicio.entity.Usuarios;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuariosRepository extends JpaRepository<Usuarios, Integer> {
-}
